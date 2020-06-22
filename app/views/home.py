084c9b8753382aca84efe5bcb26c04ceddfe565e
Marcus Pereira <mpereirassa@gmail.com>
2020-06-22 04:22:47 -0300

    .feat: puxando todos os cursos do db e adicionando na home

--- app/views/home.py
+++ app/views/home.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template
 
+from app.models import Course
 
 home = Blueprint("home", __name__)
 
 
 @home.route("/")
 def page():
-    return render_template("pages/home.html")
+    courses = Course.query.all()
+    return render_template("pages/home.html", courses=courses)
