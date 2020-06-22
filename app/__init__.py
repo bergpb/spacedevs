2d1f2583760a8b1f2a55e8b195eaeda3d5779d06
Marcus Pereira <mpereirassa@gmail.com>
2020-06-22 03:34:23 -0300

    registrando admin plugin

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,21 @@
-from flask import Flask
 from dynaconf import FlaskDynaconf
-from . import routes
+from flask import Flask
+from flask_admin import Admin
+from flask_sqlalchemy import SQLAlchemy
 
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     FlaskDynaconf(app)
 
+    db.init_app(app)
+
+    from . import admin, routes
+
+    admin_registered = Admin(app)
     routes.init_app(app)
+    admin.init_app(admin_registered)
     
     return app
