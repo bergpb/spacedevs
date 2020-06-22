8e72a837849c8c01c79d332c64762e8f9363533a
Marcus Pereira <mpereirassa@gmail.com>
2020-06-22 03:13:24 -0300

    .feat: adicionando rotas para cursos

--- app/routes.py
+++ app/routes.py
@@ -2,3 +2,4 @@ from . import views
 
 def init_app(app):
     app.register_blueprint(views.home)
+    app.register_blueprint(views.course)
--- app/views/course.py
+++ app/views/course.py
@@ -1,6 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
-course = Blueprint("course", __name__)
+course = Blueprint("course", __name__, url_prefix="/cursos")
 
 
+@course.route("/")
+def catalogue():
+    return render_template("pages/course/catalogue.html")
+
+
+@course.route("/<slug>")
+def single():
+    return render_template("pages/course/single.html")
