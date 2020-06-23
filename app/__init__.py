ede320ed40e41db4aefc8363e55753d4377b514b
Marcus Pereira <mpereirassa@gmail.com>
2020-06-23 01:53:48 -0300

    refatorando __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,23 @@
 from dynaconf import FlaskDynaconf
 from flask import Flask
-from flask_admin import Admin
-from flask_babel import Babel
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+dynaconf = FlaskDynaconf()
+
 login_manager = LoginManager()
+login_manager.login_view = "login"
+login_manager.login_message_category = "error"
+login_manager.login_message = "Você deve fazer o login antes de continuar."
 
 
 def create_app():
     app = Flask(__name__)
-    FlaskDynaconf(app)
-    Babel(app)
+    dynaconf.init_app(app)
+
+    app.config.load_extensions()
     login_manager.init_app(app)
-
     db.init_app(app)
-
-    from . import admin, routes, filters
-
-    admin_registered = Admin(app)
-    routes.init_app(app)
-    admin.init_app(admin_registered)
-
-    # registered filters
-    app.jinja_env.filters["format_time_to_string"] = filters.convert_time_to_string
 
     return app
