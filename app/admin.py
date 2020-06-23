cbbd86fb3f57b0fb292b98a0b9391d790eb6f7ce
Marcus Pereira <mpereirassa@gmail.com>
2020-06-23 01:52:13 -0300

    configurado pelo dynaconf

--- app/admin.py
+++ app/admin.py
@@ -1,13 +1,37 @@
+from flask import redirect, url_for
+from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
+from flask_admin.menu import MenuLink
+from flask_login import current_user
 from werkzeug.security import generate_password_hash
 from wtforms.fields import PasswordField
-from flask_admin import Admin
 
 from . import db
 from .models import Course, Quote, Tag, User
 
 
-class UserView(ModelView):
+class MyAdminView(AdminIndexView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.is_admin
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for("login.page"))
+
+
+class BaseViewAuthRequired(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.is_admin
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for("login.page"))
+
+
+class LogoutMenuLink(MenuLink):
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+
+class UserView(BaseViewAuthRequired):
     form_extra_fields = {"password": PasswordField("Password")}
     form_edit_columns = ("first_name", "last_name", "email")
     column_exclude_list = ["password"]
@@ -18,8 +42,28 @@ class UserView(ModelView):
             model.password = generate_password_hash(form.password.data)
 
 
-def init_app(admin: Admin):
+class CourseView(BaseViewAuthRequired):
+    pass
+
+
+class TagView(BaseViewAuthRequired):
+    pass
+
+
+class QuoteView(BaseViewAuthRequired):
+    pass
+
+
+def init_app(app):
+    admin = Admin(
+        app,
+        url=app.config.ADMIN_URL,
+        name=app.config.FLASK_ADMIN_NAME,
+        index_view=MyAdminView(url=app.config.ADMIN_URL),
+    )
+
     admin.add_view(UserView(User, db.session))
-    admin.add_view(ModelView(Course, db.session))
-    admin.add_view(ModelView(Quote, db.session))
-    admin.add_view(ModelView(Tag, db.session))
+    admin.add_view(CourseView(Course, db.session))
+    admin.add_view(QuoteView(Quote, db.session))
+    admin.add_view(TagView(Tag, db.session))
+    admin.add_link(LogoutMenuLink(name="Logout", url="/logout"))
--- app/filters.py
+++ app/filters.py
@@ -3,3 +3,8 @@ from datetime import timedelta
 
 def convert_time_to_string(seconds):
     return str(timedelta(seconds=int(seconds)))
+
+
+def init_app(app):
+    app.jinja_env.filters["format_time_to_string"] = convert_time_to_string
+
