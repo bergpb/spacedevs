5a528d7a38699c3893312b6b2a6ffea430d5c2a5
Marcus Pereira <mpereirassa@gmail.com>
2020-06-23 23:35:19 -0300

    removendo linha em branco

--- app/__init__.py
+++ app/__init__.py
@@ -15,8 +15,8 @@ login_manager.login_message = "Você deve fazer o login antes de continuar."
 def create_app():
     app = Flask(__name__)
     dynaconf.init_app(app)
-
     app.config.load_extensions()
+
     login_manager.init_app(app)
     db.init_app(app)
 
