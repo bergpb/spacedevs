ac667c5e43fe2abf8108c1f210e1acfc47f8334e
Marcus Pereira <mpereirassa@gmail.com>
2020-06-25 18:52:13 -0300

    criando comando para criar usuários

---
+++ app/commands.py
@@ -0,0 +1,33 @@
+import click
+from flask.cli import with_appcontext
+from werkzeug.security import generate_password_hash
+
+from app import db as database
+from app.models import User
+
+
+@click.group()
+def db():
+    """ Managment Database  """
+    pass
+
+
+@db.command("create")
+@click.option("--name", help="Your name", required=True)
+@click.option("--email", help="Your e-mail address", required=True)
+@click.option("--password", help="Your password", required=True)
+@with_appcontext
+def create(name, last_name, email, cpf, password):
+    """ Create account """
+    user = User(
+        name=name,
+        email=email,
+        password=generate_password_hash(password),
+        is_admin=True,
+    )
+    database.session.add(user)
+    database.session.commit()
+
+
+def init_app(app):
+    app.cli.add_command(db)
